253f956f581ad5ac1380abe2625d270a1a07dd5a
keerthi561 <17wh1a0561@bvrithyderabad.edu.in>
2020-09-18 20:15:31 +0530

    printing data with the given data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { UserComponent } from './example/user.component';
+
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HelloComponent , UserComponent],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
